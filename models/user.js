2524b57112b6d85a1aa3af2ecc39b4d17326d1b5
sneha <snehavenkatesh885@gmail.com>
2025-09-18 20:52:44 +0530

    Fix: removed extra coma

--- models/user.js
+++ models/user.js
@@ -4,10 +4,10 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    age: { type: Number, required: true },
-    gender: { type: String, required: true },
-    phone: { type: String, required: true },
-    role: { type: String, enum: ["user", "admin"], default: "user" }, // 👈 important
+    age: { type: Number },
+    gender: { type: String },
+    phone: { type: String },
+    role: { type: String, enum: ["user", "admin"], default: "user" } // ✅ important
 });
 
 module.exports = mongoose.model("User", UserSchema);
